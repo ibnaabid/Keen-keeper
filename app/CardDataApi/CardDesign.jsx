56fa098578bcd7523a0aca4f2fa0970ad1bd27c1
MD Ibna Abid <mdabout2@gmail.com>
2026-04-14 00:59:02 +0600

    add dynamic cards

--- app/CardDataApi/CardDesign.jsx
+++ app/CardDataApi/CardDesign.jsx
@@ -1,13 +1,64 @@
 
+import Image from "next/image";
+import Link from "next/link";
 
-const CardDesign = ({allClient}) => {
-    const{bio,id,name,picture,email,status,tags,goal,next_due_date,days_since_contact}=allClient;
+const CardDesign = ({ allClient }) => {
+  const {
+    id,
+    name,
+    picture,
+    email,
+    days_since_contact,
+    status,
+    tags,
+    bio,
+    goal,
+    next_due_date
+  } = allClient;
 
-    return (
-        <div>
-            <h2>{allClient.bio}</h2>
+  const statusColor = 
+    status === "overdue" ? "badge-error" :
+    status === "almost due" ? "badge-warning" : 
+    "badge-success";
+
+  return (
+    <Link href={`/blogs/${id}`} className="card bg-base-100 shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 overflow-hidden border border-base-200">
+      
+      <figure className="relative h-64 bg-base-200">
+        <Image
+              
+          src={picture}
+          alt={name}
+          width={180}
+          height={50}
+        className="rounded-full bg-cover"
+        >
+
+        </Image>
+        
+      </figure>
+<div className="absolute top-4 right-4">
+          <div className={`badge ${statusColor} badge-md font-medium px-4 py-1 shadow-md`}>
+            {status.toUpperCase()}
+          </div>
         </div>
-    );
+      <div className="card-body p-6">
+      
+        <div className="flex justify-between items-center ">
+          <h2 className="card-title text-2xl text-center font-semibold leading-tight">
+            {name}
+          </h2>
+ <div className="badge badge-neutral badge-sm font-medium">
+            {days_since_contact} days
+          </div>
+         
+          
+        </div>
+      </div>
+      <div className="btn btn-success flex gap-3 px-5 text-center"
+      >{tags}</div>
+    </Link>
+  );
 };
 
 export default CardDesign;
